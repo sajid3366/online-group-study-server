55ffe57c55960af19916ebc6701a9bcec14e6ec4
sajid360 <ahteshamsajid8@gmail.com>
2023-11-05 01:19:49 +0600

    creat assignment and post to the server added

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -7,9 +9,98 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 
-app.get("/", (req, res) =>{
-    res.send("online study group server is runnig");
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.l9mlnno.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+    const userCollection = client.db("studygroupDB").collection("user")
+    const assignmentCollection = client.db("studygroupDB").collection("assignment")
+
+    // for user collection
+    app.get('/user', async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    app.post('/user', async (req, res) => {
+      const newUser = req.body;
+      const result = await userCollection.insertOne(newUser);
+      res.send(result)
+    })
+
+    // for create assignment
+    app.get('/assignment', async (req, res) => {
+      const cursor = assignmentCollection.find();
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    app.post('/assignment', async (req, res) => {
+      const newUser = req.body;
+      const result = await assignmentCollection.insertOne(newUser);
+      res.send(result)
+    })
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://<automotiveCarHant>:<0TC2zduR3ASj24jq>@cluster0.l9mlnno.mongodb.net/?retryWrites=true&w=majority";
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     // await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+
+
+app.get("/", (req, res) => {
+  res.send("online study group server is runnig");
 })
 app.listen(port, () => {
-    console.log(`server is runnig on port: ${port}`);
+  console.log(`server is runnig on port: ${port}`);
 })
